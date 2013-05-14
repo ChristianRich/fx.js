742687813036c728448d63d575ee4b847caf6197
Christian Schlosrich <christian.schlosrich@make.com.au>
2013-05-14 13:52:18 +1000

    Update

--- src/utils/QueryString.js
+++ src/utils/QueryString.js
@@ -1,32 +1,96 @@
 /**
-* Utility for accessing query strings in the current url.
+* Utility for accessing query strings from the current url.
 */
+
 fx.QueryString = {
 
     /**
     * Returns the value of the requested key if it exists, otherwise returns null.
-    * @param  {String} fn Keyname
-    * @return {String} value
+    * @param  {String} value Key value
+    * @return {String | Boolean} value
     */
-    get : function(fn){
-        var name = fn.replace(/[\[]/, '\\\[').replace(/[\]]/, '\\\]'),
-            regexS = '[\\?&]' + name + '=([^&#]*)',
-            regex = new RegExp(regexS),
-            results = regex.exec(window.location.search);
+    getValue : function(value){
 
-        if(!results){
-            return null;
-        } else{
-            return decodeURIComponent(results[1].replace(/\+/g, ' '));
-        }
+		if(!value){
+			return false;
+		}
+
+		var r = this.parse();
+		return r[value] ? r[value] : null;
     },
 
     /**
-    * Returns true if the requested query string is present, no matter the value of the key. Otherwise returns false.
-    * @param  {String} fn Keyname
+    * Returns true if the requested query string key name exists, no matter the value of the key. Otherwise returns false.
+    * @param  {String} value Key value
     * @return {Boolean} result
     */
-    has : function(fn){
-        return !!this.get(fn);
-    }
-}
+    hasKey : function(value){
+        return !!this.getValue(value);
+    },
+
+	/**
+	* Returns the number of key / value pairs in the query string
+	* @returns {number}
+	*/
+	getLength : function(){
+
+		var r = this.parse(),
+			len = 0;
+
+		for(var prop in r){
+			len++;
+		}
+
+		return len;
+	},
+
+	/**
+	* Dumps the key / value pairs into a readable string (for debug and logging purposes)
+	*/
+	dump : function(){
+
+		var len = this.getLength(), r, s, c;
+
+		if(len == 0){
+			return 'No query string found in url';
+		} else{
+			s = 'Query string contains ' + len + ' key / value pairs:\n';
+		}
+
+		r = this.parse();
+		c = 0;
+
+		for(var i in r){
+			if(r[i]){
+				s += '[' + c.toString() + '] ' + i + ': ' + r[i] + '\n';
+				c++;
+			}
+		}
+
+		return s;
+	},
+
+	/**
+	* Return the query string as on object
+	* @return {object}
+	*/
+	parse : function(){
+
+		var dict  = {},
+			decode = function (s) {
+				return decodeURIComponent(s.replace(/\+/g, " "));
+			},
+			queryString = location.search.substring(1),
+			keyValues = queryString.split('&');
+
+		for(var i in keyValues) {
+			var key = keyValues[i].split('=');
+
+			if(key.length > 1) {
+				dict[decode(key[0])] = decode(key[1]);
+			}
+		}
+
+		return dict;
+	}
+};
