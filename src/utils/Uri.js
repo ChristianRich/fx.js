86a5ef37709f2c5e33da10af374e788fe5eeb405
Christian Schlosrich <christianschlosrich@peoplebrowsr.com>
2014-09-26 16:40:02 +1000

    RegEx validation

--- src/utils/Uri.js
+++ src/utils/Uri.js
@@ -7,13 +7,21 @@ fx.Uri = {
     
     /**
     * Returns true if supplied uri is a properly formatted url
-    * @param  {string} s
+    * @param  {string} str
     * @return {boolean}
     */
-    isUrl : function(s){
-        return s.indexOf('http') === 0 || s.indexOf('www') === 0;
+    isUrl : function (str) {
+
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+        return pattern.test(str);
     },
-    
+
     /**
     * Returns the file extension of a uri (if present)
     * @param  {string} uri 
